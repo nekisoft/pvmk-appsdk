49ecb28dc526e143d91aad5f0cdd4e2eeca35971
Bryan E. Topp <betopp@betopp.com>
2024-09-08 22:06:04 +0800

    Working on consumer version... might just make it the new dev ver too

---
+++ src/updates_build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#updates_build.sh
+#Makes updates package for PVMK application SDK
+#Bryan E. Topp <betopp@betopp.com> 2024
+
+mkdir -p out/updates
+cp -r src/updates/* out/updates/
+
