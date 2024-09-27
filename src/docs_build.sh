8bbdf2392b3e2d94fdae7a19997902b099a5ceab
Bryan E. Topp <betopp@betopp.com>
2024-09-27 23:38:49 +0800

    Docs update

--- src/docs_build.sh
+++ src/docs_build.sh
@@ -5,14 +5,16 @@
 
 
 mkdir -p out/docs
+TMPDIR=$(mktemp -d)
 OUTDIR=$(readlink -f out/docs)
 
 pushd src/docs/intro
 #Twice to update refs
-pdflatex  -output-directory=${OUTDIR} -halt-on-error intro.latex
-pdflatex  -output-directory=${OUTDIR} -halt-on-error intro.latex
+pdflatex  -output-directory=${TMPDIR} -halt-on-error intro.latex
+pdflatex  -output-directory=${TMPDIR} -halt-on-error intro.latex
 
 
+cp ${TMPDIR}/*.pdf ${OUTDIR}
 
 
 
