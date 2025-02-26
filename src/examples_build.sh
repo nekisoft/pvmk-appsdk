c1b474298f58ccba356f01cf4eaa2b8bc770e431
Bryan E. Topp <betopp@betopp.com>
2025-02-26 23:25:28 +0800

    Copy out all symlinked files, in full, when making SDK. Helps a ton on Windows.

--- src/examples_build.sh
+++ src/examples_build.sh
@@ -3,6 +3,8 @@
 #Copies examples for PVMK application SDK
 #Bryan E. Topp <betopp@betopp.com> 2024
 
+#Follow all symlinks in the copy.
+#This blows up the size of the output a bit, but makes it far far more stable on Windows environments
 mkdir out/examples
-cp -rP src/examples/* out/examples/
+cp -rL src/examples/* out/examples/
 
