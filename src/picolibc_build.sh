d4e036e236751f712ac1352b9c73de969dd3c6e5
Bryan E. Topp <betopp@betopp.com>
2025-05-17 00:34:12 +0800

    Turn on right optimization setting in picolibc build - whoops. memcpy and friends no longer pathologically slow.

--- src/picolibc_build.sh
+++ src/picolibc_build.sh
@@ -8,7 +8,7 @@ popd
 
 mkdir obj/picolibc-build
 pushd obj/picolibc-build
-../picolibc-1.8.6/scripts/do-arm-configure -Dc_args=" -fno-short-enums -fsigned-char " -Dnewlib-mb=true -Dnewlib-locale-info=true -Dthread-local-storage=false -Dmultilib=false -Dsemihost=false -Dtests=false -Dpicocrt=false -Dpicolib=false -Dposix-console=true -Dprefix=`readlink -f ../../out` -Dspecsdir=`readlink -f ../../out`/picolibc/arm-none-eabi/lib
+../picolibc-1.8.6/scripts/do-arm-configure -Dc_args=" -fno-short-enums -fsigned-char " -Dnewlib-mb=true -Dnewlib-locale-info=true -Dthread-local-storage=false -Dmultilib=false -Dsemihost=false -Dtests=false -Dpicocrt=false -Dpicolib=false -Dposix-console=true -Dprefix=`readlink -f ../../out` -Dspecsdir=`readlink -f ../../out`/picolibc/arm-none-eabi/lib -Doptimization=2
 ninja
 ninja install
 popd
