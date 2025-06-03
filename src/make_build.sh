4ed4ec5c4b7f4e2b5b6dbcb30099403f41d51187
Bryan E. Topp <betopp@betopp.com>
2025-06-03 20:35:21 +0800

    Don't need plugin API header from Make

--- src/make_build.sh
+++ src/make_build.sh
@@ -17,7 +17,7 @@ cd make-build
 OUTDIR=$(readlink -f ../..)/out
 PLATDIR=${OUTDIR}/bin/$(uname -o)/$(uname -m)/
 
-../make-4.4.1/configure MAKEINFO="true" LDFLAGS="--static" --prefix=${OUTDIR} --disable-nls --program-prefix=pvmk- --bindir=${PLATDIR} --libdir=${PLATDIR} --libexecdir=${PLATDIR} --mandir=${OUTDIR}/trash --infodir=${OUTDIR}/trash --docdir=${OUTDIR}/trash --without-guile 
+../make-4.4.1/configure MAKEINFO="true" LDFLAGS="--static" --prefix=${OUTDIR} --disable-nls --program-prefix=pvmk- --bindir=${PLATDIR} --libdir=${PLATDIR} --libexecdir=${PLATDIR} --includedir=${OUTDIR}/trash --mandir=${OUTDIR}/trash --infodir=${OUTDIR}/trash --docdir=${OUTDIR}/trash --without-guile 
 
 MAKE=gmake
 if [ "$(which ${MAKE})" == "" ]
