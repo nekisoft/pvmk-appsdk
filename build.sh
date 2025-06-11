27de622804c469b9c1d50370a44b311966539e51
Bryan E. Topp <betopp@betopp.com>
2025-06-11 13:16:41 +0800

    Give up porting Toybox, start with individual utils from FreeBSD instead

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ mkdir -p obj
 ./src/make_build.sh
 ./src/xorriso_build.sh
 ./src/sdlsc_build.sh
-./src/toybox_build.sh
+./src/bsdutils_build.sh
 
 #Binutils and compiler for targetting PVMK
 ./src/wrappers_build.sh
---
+++ src/bsdutils_build.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#bsdutils_build.sh
+#Makes subset of BSD Utilities for PVMK application SDK
+#Bryan E. Topp <betopp@betopp.com> 2025
+
+set -x
+set -e
+
+mkdir -p obj/bsdutils
+pushd obj/bsdutils
+
+SRCDIR="../../src/bsdutils"
+
+CC=gcc
+
+CFLAGS+=" -I${SRCDIR}"
+CFLAGS+=" -g -O2"
+CFLAGS+=" -std=c99 -Wall -Wextra -Werror "
+CFLAGS+=" -Wno-clobbered "
+CFLAGS+=" -D_POSIX=1 "
+
+OUTDIR=../../out
+PLATDIR=${OUTDIR}/bin/$(uname -o)/$(uname -m)
+mkdir -p ${PLATDIR}
+
+${CC} ${SRCDIR}/date/*.c -o ${PLATDIR}/pvmk-date -static
+${CC} ${SRCDIR}/cat/*.c -o ${PLATDIR}/pvmk-cat -static
+${CC} ${SRCDIR}/find/*.c -o ${PLATDIR}/pvmk-find -static
+
+popd
