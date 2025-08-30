df8efa5c799ace6faa3c16cac2b5366230a4e1b3
Bryan E. Topp <betopp@betopp.com>
2025-08-31 00:39:31 +0800

    Fixing up font generator

--- src/examples/goofs/soccest/art/genfont.sh
+++ src/examples/goofs/soccest/art/genfont.sh
@@ -7,12 +7,13 @@ do
 	do
 		if [ ${HHA}${HHB} != 00 ]
 		then
-			echo 00 ${HHA}${HHB} | xxd -rg1  | magick -background transparent -fill black -font "Helvetica" -size 32x32 -pointsize 32 -gravity center label:@- ${TMPD}/${HHA}${HHB}.png 
+			echo 00 ${HHA}${HHB} | xxd -rg1  | magick -background black -fill white -font "Helvetica" -size 32x32 -pointsize 32 -gravity west label:@- ${TMPD}/${HHA}${HHB}.png 
 		fi
 	done
 done
 cp ${TMPD}/01.png ${TMPD}/00.png
 ls ${TMPD}/*.png | awk '{print $0}END{print "font.png"}' | xargs magick convert -append
+convert font.png font.xbm
 rm ${TMPD}/*.png
 rmdir ${TMPD}
 
