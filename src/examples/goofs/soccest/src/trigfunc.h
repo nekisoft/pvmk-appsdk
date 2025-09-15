eb5592998cb7e86c4792d1f4cba4069c6b7ce9e6
Bryan E. Topp <betopp@betopp.com>
2025-09-16 02:34:48 +0800

    Adding atan2 trig table

--- src/examples/goofs/soccest/src/trigfunc.h
+++ src/examples/goofs/soccest/src/trigfunc.h
@@ -12,4 +12,7 @@ int32_t trigfunc_sin8(int32_t input_ang16);
 //Returns 0.8 cosine of 0.16 angle (65536 = 360 degrees)
 int32_t trigfunc_cos8(int32_t input_ang16);
 
+//Returns 0.16 angle for given slope
+int32_t trigfunc_atan2(int32_t y, int32_t x);
+
 #endif //TRIGFUNC_H
